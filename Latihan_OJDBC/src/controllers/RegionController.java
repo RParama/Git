0265b5918dba7430bac3353f8b06dc6cd376d994
cahyaputri <elkecahyaa@gmail.com>
2019-01-27 22:28:37 +0700

    Update RegionController.java

--- Latihan_OJDBC/src/controllers/RegionController.java
+++ Latihan_OJDBC/src/controllers/RegionController.java
@@ -18,10 +18,23 @@ public class RegionController {
     private RegionDAO rdao;
     private Connection connection;
 
+     /**
+     * Method RegionController merupakan constructor method dari class RegionController
+     * method yang pertama kali dijalankan ketika class di eksekusi
+     * Pada method ini melakukan instansiasi objek 
+     * bernama rdao pada class RegionDAO untuk melakukan koneksi ke database
+     * Method RegionController memiliki access modifier public
+     */
     public RegionController(Connection connection) {
         rdao = new RegionDAO(connection);
     }
 
+    /**
+     * Method insert digunakan untuk melakukan insert data baru ke database
+     * dengan cara memanggil method save dengan kondisi true pada class RegionDAO
+     * Method insert memiliki parameter id dan name yang memiliki tipe data string
+     * Method insert memiliki access modifier public
+     */
     public boolean insert(String id, String name) {
         boolean result = false;
         try {
@@ -34,6 +47,12 @@ public class RegionController {
         return result;
     }
     
+    /**
+     * Method update digunakan untuk melakukan insert data baru ke database
+     * dengan cara memanggil method save dengan kondisi false pada class RegionDAO
+     * Method update memiliki parameter id dan name yang memiliki tipe data string
+     * Method update memiliki access modifier public
+     */
     public boolean update(String id, String name) {
         boolean result = false;
         try {
