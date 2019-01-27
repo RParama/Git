feef436bd484c7f18cafe0e575d4c7a6ed2fe615
cahyaputri <elkecahyaa@gmail.com>
2019-01-27 22:14:19 +0700

    Update JobController.java

--- Latihan_OJDBC/src/controllers/JobController.java
+++ Latihan_OJDBC/src/controllers/JobController.java
@@ -21,10 +21,23 @@ public class JobController {
     private JobDAO jdao;
     private Connections connection;
 
+    /**
+     * Method JobController merupakan constructor method dari class JobController
+     * method yang pertama kali dijalankan ketika class di eksekusi
+     * Pada method ini melakukan instansiasi objek 
+     * bernama jdao pada class JobDAO untuk melakukan koneksi ke database
+     * Method JobController memiliki access modifier public
+     */
     public JobController(Connection connection) {
         jdao = new JobDAO(connection);
     }
-    
+ 
+     /**
+     * Method getJobs digunakan untuk menampilkan data pada jobs secara keseluruhan
+     * dengan cara memanggil method getData dengan kondisi false pada class JobDAO
+     * Method getJobs memiliki list bernama joblist yang berfungsi untuk menyimpan hasil pencarian
+     * Method getJobs memiliki access modifier public
+     */
     public List<String> getJobs (){
         List<String> joblist = new ArrayList<String>();
         boolean result = false;
@@ -41,6 +54,13 @@ public class JobController {
         return joblist;
     }
     
+     /**
+     * Method insert digunakan untuk melakukan insert data baru ke database
+     * dengan cara memanggil method save dengan kondisi true pada class JobDAO
+     * Method insert memiliki parameter id dan title bertipe data string
+     * minSalary dan maxSalary bertipe data integer
+     * Method insert memiliki access modifier public
+     */
     public boolean insert(String id, String title, int minSalary, int maxSalary) {
         boolean result = false;
         try {
@@ -53,6 +73,13 @@ public class JobController {
         return result;
     }
 
+    /**
+     * Method update digunakan untuk melakukan update data pada database
+     * dengan cara memanggil method save dengan kondisi false pada class JobDAO
+     * Method update memiliki parameter id dan title bertipe data string
+     * minSalary dan maxSalary bertipe data integer
+     * Method update memiliki access modifier public
+     */
     public boolean update(String id, String title, int minSalary, int maxSalary) {
         boolean result = false;
         try {
@@ -65,6 +92,12 @@ public class JobController {
         return result;
     }
 
+     /**
+     * Method delete digunakan untuk menghapus department berdasarkan job id
+     * dengan cara memanggil method delete pada class JobDAO
+     * Method delete memiliki parameter id bertipe data string
+     * Method delete memiliki access modifier public
+     */
     public boolean delete(String id) {
         boolean result = false;
         try {
@@ -76,6 +109,12 @@ public class JobController {
         return result;
     }
 
+    /**
+     * Method getData digunakan untuk menampilkan data pada department secara keseluruhan
+     * dengan cara memanggil method getData dengan kondisi false pada class DepartmentDAO
+     * Method getData memiliki list bernama job yang berfungsi untuk menyimpan hasil pencarian
+     * Method getData memiliki access modifier public
+     */
     public List<Job> getData() {
         List<Job> job = new ArrayList<Job>();
  
@@ -90,6 +129,12 @@ public class JobController {
         return job;
     }
 
+     /**
+     * Method getById digunakan untuk melakukan pencarian data berasarkan job id
+     * dengan cara memanggil method getData dengan kondisi false pada class JobDAO
+     * Method getById memiliki list bernama job yang berfungsi untuk menyimpan hasil pencarian
+     * Method getById memiliki access modifier public
+     */
     public List<Job> getById(Object id) {
         List<Job> job = new ArrayList<Job>();
         boolean result = false;
@@ -103,6 +148,13 @@ public class JobController {
         return job;
     }
 
+    /**
+     * Mendeklarasikan method main yang berisi :
+     * Instansiasi objek bernama connections pada class Connections
+     * Instansiasi obje bernama a pada class JobController
+     * Memanggil method getJobId dan getJobTitle pada class model Job
+     * untuk menampilkan id dana nama
+     */
     public static void main(String[] args) {
         Connections connections = new Connections();
         JobController a = new JobController(connections.getConnection());
