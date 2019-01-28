f8ce14c5bb8e23a354fc3769b688c23308c9daf9
cahyaputri <elkecahyaa@gmail.com>
2019-01-28 22:21:13 +0700

    Update LocationController.java

--- Latihan_OJDBC/src/controllers/LocationController.java
+++ Latihan_OJDBC/src/controllers/LocationController.java
@@ -21,23 +21,21 @@ public class LocationController {
     private LocationDAO ldao;
     private Connection connection;
 
-    /**
-     * Method LocationController merupakan constructor method dari class
-     * LocationController method yang pertama kali dijalankan ketika class di
-     * eksekusi Pada method ini melakukan instansiasi objek bernama ldao pada
-     * class LocationDAO untuk melakukan koneksi ke database Method
-     * LocationController memiliki access modifier public
+     /**
+     * Pada method ini melakukan instansiasi objek 
+     * bernama ldao pada class LocationDAO untuk melakukan koneksi ke database
+     * @param connection objek dari class Connection
      */
     public LocationController(Connection connection) {
         ldao = new LocationDAO(connection);
     }
 
     /**
-     * Method getDataById digunakan untuk melakukan pencarian data berasarkan
-     * location id dengan cara memanggil method getData dengan kondisi true pada
-     * class LocationDAO Method getDataById memiliki list bernama locations yang
-     * berfungsi untuk menyimpan hasil pencarian Method getDataById memiliki
-     * access modifier public
+     * Fungsi digunakan untuk melakukan pencarian data berasarkan id 
+     * dengan cara memanggil method getData dengan kondisi true pada class LocationDAO 
+     * List bernama locations berfungsi untuk menyimpan hasil pencarian
+     * @param id bertipe data string
+     * @return menampilkan data berdasarkan id yang dicari
      */
     public List<Location> getDataById(String id) {
         List<Location> locations = new ArrayList<Location>();
@@ -51,12 +49,10 @@ public class LocationController {
     }
 
     /**
-     * Method getDataSearch digunakan untuk melakukan pencarian data pada
-     * departments sesuai dengan keyword yang di inputkan dengan cara memanggil
-     * method getData dengan kondisi false pada class LocationDAO Method
-     * getDataSearch memiliki list bernama locations yang berfungsi untuk
-     * menyimpan hasil pencarian Method getDataSearch memiliki access modifier
-     * public
+     * Fungsi digunakan untuk melakukan pencarian data sesuai dengan keyword
+     * Memanggil method getData dengan kondisi false pada class LocationDAO
+     * @param k bertipe Obejct
+     * @return menampilkan data yang di cari
      */
     public List<Location> getDataSearch(Object k) {
         List<Location> locations = new ArrayList<Location>();
@@ -69,11 +65,10 @@ public class LocationController {
     }
 
     /**
-     * Method insert digunakan untuk melakukan insert data baru ke database
-     * dengan cara memanggil method save dengan kondisi true pada class
-     * LocationDAO Method insert memiliki parameter locationId, streetAddress,
-     * postalCode, city dan countryId yang memiliki tipe data string Method
-     * insert memiliki access modifier public
+     * Fungsi digunakan untuk melakukan insert data 
+     * Memanggil method save dengan kondisi true pada class LocationDAO 
+     * @param locationId, streetAddress, postalCode, city, stateProvince, countryId bertipe string
+     * @return berhasil melakukan insert data
      */
     public boolean insert(String locationId, String streetAddress, String postalCode,
             String city, String stateProvince, String countryId) {
@@ -90,11 +85,10 @@ public class LocationController {
     }
 
     /**
-     * Method update digunakan untuk melakukan update data baru ke database
-     * dengan cara memanggil method save dengan kondisi false pada class
-     * LocationDAO Method update memiliki parameter locationId, streetAddress,
-     * postalCode, city dan countryId yang memiliki tipe data string Method
-     * update memiliki access modifier public
+     * Fungsi digunakan untuk melakukan update data 
+     * Memanggil method save dengan kondisi false pada class LocationDAO 
+     * @param id bertipe locationId, streetAddress, postalCde, city, stateProvince, countryId bertipe data string
+     * @return berhasil melakukan update
      */
     public boolean update(String locationId, String streetAddress, String postalCode,
             String city, String stateProvince, String countryId) {
@@ -111,10 +105,10 @@ public class LocationController {
     }
 
     /**
-     * Method delete digunakan untuk menghapus department berdasarkan locationId
-     * dengan cara memanggil method delete pada class LocationDAO Method delete
-     * memiliki parameter locationId bertipe data string Method delete memiliki
-     * access modifier public
+     * Fungsi digunakan untuk menghapus data berdasarkan locationId
+     * Memanggil method delete pada class LocationDAO
+     * @param locationId bertipe string
+     * @return berhasil menghapus data sesuai dengan locationId
      */
     public boolean delete(String locationId) {
         boolean result = false;
